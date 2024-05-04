1e7771443b4455cb1566948fa19fcde865b3008d
YancyZhang30 <YancyZhang30@users.noreply.github.com>
2024-05-04 06:05:15 +0000

    deploy: d427d3f70968b71a7d5052ad930426cafc0428ae

---
+++ assets/5-其他_1-数据结构_4-栈.md.7264e13c.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,v as e,b as a}from"./chunks/framework.caa0fbaf.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"5-其他/1-数据结构/4-栈.md","filePath":"5-其他/1-数据结构/4-栈.md","lastUpdated":1714802656000}'),s={name:"5-其他/1-数据结构/4-栈.md"};function r(_,n,o,d,c,p){return e(),a("div")}const m=t(s,[["render",r]]);export{f as __pageData,m as default};
